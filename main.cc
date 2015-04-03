8988bd2e81dbc05e7433a6f509d922ad8e400779
Florian Paul Schmidt <mista.tapas@gmx.net>
2015-04-03 22:08:01 +0200

    fumbling around

--- main.cc
+++ main.cc
@@ -1,5 +1,13 @@
-int main()
-{
+#include <kglobalaccel.h>
+#include <QApplication>
+#include <event_filter.h>
 
+int main(int argc, char *argv[])
+{
+	QApplication app(argc, argv);
+	KGlobalAccel *k = KGlobalAccel::self();
+	event_filter f;
+	app.installEventFilter(&f);
+	return app.exec();
 }
 
